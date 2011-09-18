8e3ba4bf0031c3c7b41286fb4454fbbe4e3f1a20
tps12 <tps12@columbia.edu>
2011-09-18 13:02:33 -0400

    Process latitudes in process pool.

--- planetsimulation.py
+++ planetsimulation.py
@@ -1,5 +1,6 @@
 from cPickle import dump, load
 from math import asin, acos, atan2, pi, exp, sqrt, sin, cos
+from multiprocessing import cpu_count, Pool
 import random
 
 from numpy import *
@@ -57,6 +58,25 @@ def apply_velocity(p, v):
         return tuple([rotate(i, axis, d) for i in (p,v)])
     else:
         return p, v
+
+def _iteratelat(latdata):
+    lat, shapes = latdata
+    inlatrange = [s for s in shapes
+                  if s.latrange.contains(lat[0][0])]
+    if inlatrange:
+        cos_lat = cos(lat[0][0] * pi/180)
+        z = sin(lat[0][0] * pi/180)
+    for x in range(len(lat)):
+        value = 0
+        for s in [s for s in inlatrange
+                  if s.lonrange.contains(lat[x][1])]:
+            v = (cos_lat * cos(lat[x][1] * pi/180),
+                 cos_lat * sin(lat[x][1] * pi/180),
+                 z)
+            if s.contains(v):
+                value += 1
+        lat[x] = (lat[x][0], lat[x][1], value)
+    return lat
 
 class PlanetSimulation(object):
     
@@ -119,28 +139,13 @@ class PlanetSimulation(object):
 
             self._shapes.append(Shape(coords, p, o, v))
 
+        self._pool = Pool(processes=cpu_count())
+
     def update(self):
         for s in self._shapes:
             s.apply_velocity(0.01)
 
         shapes = [s.projection() for s in self._shapes]
 
-        for y in range(len(self.tiles)):
-            inlatrange = [s for s in shapes
-                          if s.latrange.contains(self.tiles[y][0][0])]
-            if inlatrange:
-                cos_lat = cos(self.tiles[y][0][0] * pi/180)
-                z = sin(self.tiles[y][0][0] * pi/180)
-            for x in range(len(self.tiles[y])):
-                value = 0
-                for s in [s for s in inlatrange
-                          if s.lonrange.contains(self.tiles[y][x][1])]:
-                    v = (cos_lat * cos(self.tiles[y][x][1] * pi/180),
-                         cos_lat * sin(self.tiles[y][x][1] * pi/180),
-                         z)
-                    if s.contains(v):
-                        value += 1
-                self.tiles[y][x] = (self.tiles[y][x][0],
-                                    self.tiles[y][x][1],
-                                    value)
+        self.tiles = self._pool.map(_iteratelat, [(lat, shapes) for lat in self.tiles])
         self.dirty = True
