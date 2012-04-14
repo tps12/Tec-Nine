a1a6f82b221c90660de7619ad0c098e77fe2a249
tps12 <tps12@columbia.edu>
2012-04-14 14:56:36 -0400

    Mercator projection.

--- splitdisplay.py
+++ splitdisplay.py
@@ -81,7 +81,7 @@ class SplitDisplay(QWidget):
                        
                         screen.drawImage((x + (res[0] - len(self._sim.tiles[y]))/2)*block.width(),
                                          y*block.height(), block)
-            else:
+            elif self._projection == self.PROJECT_FLAT:
                 res = len(self._sim.tiles)
                 template = QImage(size[0]/(2*res), size[1]/res, QImage.Format_RGB32)
 
@@ -114,6 +114,36 @@ class SplitDisplay(QWidget):
                                 block.fill(QColor(*color).rgb())
 
                                 screen.drawImage(xh*block.width(), y*block.height(), block)
+            else:
+                res = len(self._sim.tiles)
+                template = QImage(size[0]/res, size[1]/res, QImage.Format_RGB32)
+
+                for y in range(res):
+                    for x in range(res):
+                        block = template.copy()
+
+                        r = self.rotate
+                        o = r * res/360
+                        xo = (x + o) * len(self._sim.tiles[y])/res
+
+                        if xo > len(self._sim.tiles[y])-1:
+                            xo -= len(self._sim.tiles[y])
+                        elif xo < 0:
+                            xo += len(self._sim.tiles[y])
+                        h = self._sim.tiles[y][xo].value
+
+                        if h > 0:
+                            r = g = b = 128
+                            if h == 2:
+                                r = 255
+                            elif h == 3:
+                                b = 255
+                            color = (r, g, b)
+                        else:
+                            color = (255, 255, 255)
+                        block.fill(QColor(*color).rgb())
+
+                        screen.drawImage(x*block.width(), y*block.height(), block)
 
             screen.end()
 
