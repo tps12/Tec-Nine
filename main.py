db4f8d2dbf2be2577feb2192a9954ec559c60622
tps12 <tps12@columbia.edu>
2012-10-02 07:58:49 -0400

    Add default window size based on font metrics
    I had always been testing with this, so might as well commit it.

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from PySide.QtGui import QApplication, QFont, QFontMetrics, QX11Info
 from mainwindow import MainWindow
 
 app = QApplication(argv)
-#QX11Info.setAppDpiX(0, 139)
-#QX11Info.setAppDpiY(0, 144)
 font = QFont(QApplication.font())
 font.setPointSize(9)
 QApplication.setFont(font)
 w = MainWindow()
 metrics = QFontMetrics(font)
-#w.setFixedSize(metrics.width('M') * 80, metrics.height() * 25)
+w.resize(metrics.width('M') * 80, metrics.height() * 24)
 w.show()
 exit(app.exec_())
